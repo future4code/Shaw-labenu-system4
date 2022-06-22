3a95c094719d6e2cd708b86ba7dbe7e0e6dce276
Rodrigo Santos <rodrigo.v.santos89@gmail.com>
2022-06-22 17:10:48 -0300

    Fiz todas as classes

---
+++ src/Hobby.ts
@@ -0,0 +1,10 @@
+export class Hobby {
+    private name: string
+    private id: string
+
+    constructor(name: string, id: string){
+        this.name = name
+        this.id = id
+    }
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-import { app } from "./app";
+import { Turma } from "./Turma";
 
 console.log("Hello World!");
+
+const shaw = new Turma("1", "Shaw", 0);
+
+console.log("id", shaw.getId());
+
+console.log("Nome", shaw.getNome());
+
+console.log("Modulo", shaw.getModulo());
+
+//--------------------------------------//
+
+const silveira = new Turma("2", "Shaw", 0);
+
+console.log("id", silveira.getId());
+
+console.log("Nome da Turma", silveira.getNome());
+
+console.log("Modulo", silveira.getModulo());
+
+//--------------------------------------//
+
+
+
+
+
+
+
+
